60bb1c8c26c7caa93a27d676d01d90d39b328c6e
Denis <Savchenkodkz@gmail.com>
2022-12-14 05:30:38 +0600

    Задача 43

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
-﻿int InsertNum(int m){
-
-int count = 0;
-for (int i = 1; i <= m; i++ ){
-    Console.Write($"Введите число {i}: ");
-    int a = Convert.ToInt32(Console.ReadLine());
-    if (a > 0){
-        count ++;
-    }
+﻿void InsertNum(double b1, double b2, double k1, double k2){
+/*if (b1 > b2){
+    double temp = b2;
+    b2 = b1;
+    b1 = temp;
 }
-return count;
+else if (k1 > k2){
+    double temp = k2;
+    k2 = k1;
+    k1 = temp;
 }
-Console.WriteLine("Введите количество чисел");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write($"{InsertNum(m)}");
+*/
+    double x = (b2-b1) / (k1-k2);
+    double y = (k1*(b2-b1))/(k1-k2)+b1;
+    Console.Write($"[{x};{y}]");
 
+
+
+
+
+
+}
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+InsertNum(b1,b2,k1,k2);
