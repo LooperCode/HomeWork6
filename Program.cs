6d0d8f0ea43b39e28b1539ce5187be31b458b7af
Denis <Savchenkodkz@gmail.com>
2022-12-14 03:11:00 +0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int InsertNum(int m){
+
+int count = 0;
+for (int i = 1; i <= m; i++ ){
+    Console.Write($"Введите число {i}: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    if (a > 0){
+        count ++;
+    }
+}
+return count;
+}
+Console.WriteLine("Введите количество чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{InsertNum(m)}");
+
